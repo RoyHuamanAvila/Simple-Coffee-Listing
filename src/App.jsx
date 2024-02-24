d3f3cbdd72b47f0b5c11b850e797b4c144dd46c4
Roy Andres <rhavila789@gmail.com>
2024-02-24 18:25:17 -0500

    Actualizado el estilo de la aplicación y agregada la funcionalidad de cambiar la vista de productos

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,16 @@ import { ProductCard } from "./components"
 
 const App = () => {
   const [products, setProducts] = useState([])
+  const [view, setView] = useState("all_products")
 
   const fetchProducts = async () => {
     const response = await fetch("https://raw.githubusercontent.com/devchallenges-io/web-project-ideas/main/front-end-projects/data/simple-coffee-listing-data.json")
     const data = await response.json()
     setProducts(data)
+  }
+
+  const handleView = (e) => {
+    setView(e.target.id)
   }
 
   useEffect(() => {
@@ -27,16 +32,18 @@ const App = () => {
           Introducing our Coffee Collection, a selection of unique coffees from different roast types and origins, expertly roasted in small batches and shipped fresh weekly.
         </p>
         <div className="view-options">
-          <input type="radio" name="view" id="all_products" checked />
+          <input type="radio" name="view" id="all_products" onClick={handleView} defaultChecked />
           <label htmlFor="all_products">All Products</label>
 
-          <input type="radio" name="view" id="available" />
+          <input type="radio" name="view" id="available" onClick={handleView} />
           <label htmlFor="available">Available Now</label>
         </div>
 
         <section className="product-list">
           {
-            products.map((product) => (
+            view === "all_products" ? products.map((product) => (
+              <ProductCard key={product.id} {...product} />
+            )) : products.filter((product) => product.available).map((product) => (
               <ProductCard key={product.id} {...product} />
             ))
           }
